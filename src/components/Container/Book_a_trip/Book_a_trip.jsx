ac6234d1ff5713d04836f6408f870361308eb954
unknown <thaominh030617@gmail.com>
2021-07-28 14:27:36 +0700

    responsive for Book a trip & Destination

--- src/components/Container/Book_a_trip/Book_a_trip.jsx
+++ src/components/Container/Book_a_trip/Book_a_trip.jsx
@@ -4,7 +4,6 @@ import Group6 from '../../../assets/img/Group6.png'
 import Group7 from '../../../assets/img/Group7.png'
 import Group8 from '../../../assets/img/Group8.png'
 import Rectangle17 from '../../../assets/img/Rectangle 17.jpg'
-import Line from '../../../assets/img/Line 3.png'
 import Rectangle4 from '../../../assets/img/Rectangle4.png'
 import Heart from '../../../assets/img/heart.png'
 import Leaf from '../../../assets/img/LEAF.png'
@@ -30,17 +29,19 @@ function Book_a_trip() {
                     <li className="trip2-list_item">
                         <img src={Group6} alt="line-symbol" className="symbol-list" />
                         <div className="describe-list">
-                            <p style={{ fontWeight: "bold", margin: "0" }}>Choose Destination</p>
-                            <p style={{ margin: "0" }}>Lorem ipsum dolor sit amet, consectetur
+                            <p className="p-style">Choose Destination</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
                                 adipiscing elit. Urna, tortor tempus.
                             </p>
                         </div>
                     </li>
                     <li className="trip2-list_item">
                         <img src={Group7} alt="action-symbol" className="symbol-list" />
-                        <div className="describe-list font-poppin-medium">
-                            <p style={{ fontWeight: "bold", margin: "0" }}>Make Payment</p>
-                            <p style={{ margin: "0" }}>Lorem ipsum dolor sit amet, consectetur
+                        <div className="describe-list">
+                            <p className="p-style">Make Payment</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
                                 adipiscing elit. Urna, tortor tempus.
                             </p>
                         </div>
@@ -48,8 +49,9 @@ function Book_a_trip() {
                     <li className="trip2-list_item">
                         <img src={Group8} alt="car-symbol" className="symbol-list" />
                         <div className="describe-list">
-                            <p style={{ fontWeight: "bold", margin: "0" }}>Reach Airport on Selected Date</p>
-                            <p style={{ margin: "0" }}>Lorem ipsum dolor sit amet, consectetur
+                            <p className="p-style">Reach Airport on Selected Date</p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
                                 adipiscing elit. Urna, tortor tempus.
                             </p>
                         </div>
@@ -58,46 +60,43 @@ function Book_a_trip() {
             </div>
             <div className="trip3">
                 <div className="trip3-describe1">
-                    <img src={Rectangle17} alt="trip" />
-                    <p style={{ fontSize: "18px", color: "#080809", fontWeight: "bold" }}>Trip To Greece</p>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                        <p style={{ fontSize: "16px", color: "#84829A", marginRight: "3px" }}>14-29 June</p>
-                        <img src={Line} alt="line" style={{ marginRight: "20px", height: "16px" }} />
-                        <p style={{ fontSize: "16px", color: "#84829A" }}>by Robbin joseph</p>
+                    <img src={Rectangle17} alt="trip img" className="trip3-img" />
+                    <p className="p-style2">Trip To Greece</p>
+                    <div className="trip3-describe1-infor1">
+                        <p>14-29 June</p>
+                        <div className="line" />
+                        <p>by Robbin joseph</p>
                     </div>
                     <div>
-                        <img src={Leaf} alt="leaf-icon" style={{ marginRight: "20px", height: "36px" }} />
-                        <img src={Mapicon} alt="map icon" style={{ marginRight: "20px", height: "36px" }} />
-                        <img src={sendIcon1} alt="send-icon" style={{ marginRight: "20px", height: "36px" }} />
+                        <img src={Leaf} alt="leaf-icon" className="trip3-describe1-icon" />
+                        <img src={Mapicon} alt="map icon" className="trip3-describe1-icon" />
+                        <img src={sendIcon1} alt="send-icon" className="trip3-describe1-icon" />
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }} >
-                        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                            <img src={Building} alt="building" style={{ marginRight: "20px", height: "16px" }} />
-                            <p style={{ fontSize: "16px", color: "#84829A" }}>24 people going</p>
-                        </div>
-                        <img src={Heart} alt="heart icon" style={{ height: "19px", width: "20px",zIndex:"3", marginRight:"20px" }} />
-
+                    <div className="trip3-describe1-infor2">
+                        <img src={Building} alt="building" style={{ marginRight: "20px" }} />
+                        <p>24 people going</p>
+                        <img src={Heart} alt="heart icon" className="trip3-img-heart" />
                     </div>
 
                 </div>
 
-                <div className="trip3-describe2" >
+            <div className="trip4" >
                     <img src={Rectangle4} alt="rome" style={{ marginRight: "20px", height: "50px" }} />
 
-                    <div className="trip3-item">
-                        <p style={{ fontSize: "16px", color: "#84829A", margin:"0" }}>Ongoing</p>
+                    <div className="trip4-describe">
+                        <p>Ongoing</p>
 
-                        <p style={{ fontSize: "18px", color: "#080809", fontWeight: "bold", margin:"6px 0"  }}>Trip to rome</p>
+                        <p className="p-style2">Trip to rome</p>
                         <p
-                            style={{ fontSize: "16px", color: "#8A79DF", display: "flex", flexDirection: "row", alignItems: "center", margin:"10px 0 0 5px"  }}
+                            style={{ display: "flex", flexDirection: "row", alignItems: "center", margin: "10px 5px 0 5px", }}
                         >
                             40%
-                            <p style={{ fontSize: "16px", color: "#080809", fontWeight: "bold", margin: "0 5px" }}>completed</p>
+                            <p className="p-style2" style={{ fontSize: "16px", margin:"-3px 0 0 5px" }}>completed</p>
                         </p>
-                        <img src={Line2} alt="line-percent" style={{marginTop:"-10px"}} />
+                        <img src={Line2} alt="line-percent" style={{ marginTop: "-10px" }} />
 
                     </div>
-                </div>
+            </div>
                 <div className="background" />
 
             </div>
